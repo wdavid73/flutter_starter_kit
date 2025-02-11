6de82c26d54d3a89e19bc28c6739a309dfafbae5
Wilson Padilla <wdavidw26@gmail.com>
2025-02-11 11:40:05 -0500

    :adhesive_bandage: change text to 10

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -40,7 +40,7 @@ class HomeScreen extends StatelessWidget {
               onTap: () => context.push('/example_service'),
             ),
             const Spacer(),
-            Text("texto de prueba 9"),
+            Text("texto de prueba 10"),
             FilledButton.icon(
               onPressed: () => context.read<AuthBloc>().logout(),
               label: Text("LogOut"),
