f25b6f95511bce310b859dc8c4874f7f54baba08
Wilson Padilla <wdavidw26@gmail.com>
2025-02-11 10:41:43 -0500

    :adhesive_bandage: fix in workflow

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -40,7 +40,7 @@ class HomeScreen extends StatelessWidget {
               onTap: () => context.push('/example_service'),
             ),
             const Spacer(),
-            Text("texto de prueba 5"),
+            Text("texto de prueba 6"),
             FilledButton.icon(
               onPressed: () => context.read<AuthBloc>().logout(),
               label: Text("LogOut"),
