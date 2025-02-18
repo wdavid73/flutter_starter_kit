f3e1f6938f0587dff19d669240efa177d19be492
Wilson Padilla <wdavidw26@gmail.com>
2025-02-18 15:00:30 -0500

    :memo: add comments

--- lib/app/multi_repository_provider.dart
+++ lib/app/multi_repository_provider.dart
@@ -4,26 +4,41 @@ import 'package:front_scaffold_flutter_v2/data/data.dart';
 import 'package:front_scaffold_flutter_v2/domain/repositories/repositories.dart';
 import 'package:front_scaffold_flutter_v2/domain/usecases/usecases.dart';
 
+/// Builds and provides a list of `RepositoryProvider` instances.
+///
+/// This function initializes all repositories and use cases required for the app,
+/// ensuring that they are available for dependency injection via `RepositoryProvider`.
+///
+/// Returns:
+///   - A `List<RepositoryProvider>` containing all repositories and use cases.
 List<RepositoryProvider> buildRepositories() {
   return [
-    // * AUTH REPOSITORY AND DATASOURCE
+    /// Provides the [AuthRepository] and its corresponding [AuthUseCase].
+    ///
+    /// The [AuthRepository] is implemented by [AuthRepositoryImpl] and
+    /// depends on [ApiAuthDataSource] for data access.
+    /// The [AuthUseCase] depends on the [AuthRepository] for its logic.
     RepositoryProvider<AuthRepository>(
       create: (_) => AuthRepositoryImpl(
         ApiAuthDataSource(ApiClient.instance),
       ),
     ),
+
+    /// Provides `AuthUseCase`, which contains authentication-related business logic.
     RepositoryProvider<AuthUseCase>(
       create: (context) => AuthUseCase(
         context.read<AuthRepository>(),
       ),
     ),
 
-    // EXAMPLE REPOSITORY AND DATASOURCE
+    /// Provides `ProductsRepository`, which handles product-related operations.
     RepositoryProvider<ProductsRepository>(
       create: (_) => ProductsRepositoryImpl(
         ApiProductDataSource(ApiClient.instance),
       ),
     ),
+
+    /// Provides `ProductsUseCase`, which contains business logic for managing products.
     RepositoryProvider<ProductsUseCase>(
       create: (context) => ProductsUseCase(
         context.read(),
--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,15 @@ import 'package:front_scaffold_flutter_v2/app/app.dart';
 import 'package:front_scaffold_flutter_v2/config/constants/environment.dart';
 
 void main() async {
+  // Ensures that Flutter bindings are initialized before running the app.
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Locks the app orientation to portrait mode.
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
+  /// Initializes environment variables required for the app.
   await Environment.initEnvironment();
+
+  // Runs the main application widget.
   runApp(const MainApp());
 }
